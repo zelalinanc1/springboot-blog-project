22e9f6c998c69ea71d97c4011d8f70945688193c
Zelal Inanc <zelalinanc21@gmail.com>
2023-05-12 17:19:16 +0300

    first commit

---
+++ src/test/java/com/springboot/blog/BlogDemo/BlogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.BlogDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
