baf1dc5fdde64d108040a704e94117da07d4c53a
Zelal Inanc <zelalinanc21@gmail.com>
2023-05-18 12:33:12 +0300

    Spring Security config

--- src/main/java/com/springboot/blog/BlogDemo/utils/PasswordGeneratorEncoder.java
+++ src/main/java/com/springboot/blog/BlogDemo/utils/PasswordGeneratorEncoder.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordGeneratorEncoder {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode("admin"));
         System.out.println(passwordEncoder.encode("user"));
+        System.out.println(passwordEncoder.encode("admin"));
     }
 }
